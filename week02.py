988d8e7d3d1c632e6d10f3c3887db3ffa060bc58
hwchanyung <hwchanyung@gmail.com>
2026-03-12 16:22:23 +0900

    week02 v0.2

--- week02.py
+++ week02.py
@@ -1,9 +1,3 @@
 scores = [100,87,97,82]
-hap = 0
-count = 0
-for score in scores:
-    hap = hap + score
-    count = count + 1
-
-average = hap / count
+average = sum(scores)/len(scores)
 print(average)
